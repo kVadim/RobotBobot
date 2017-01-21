a1d8f6fabeda30d55b938d89f5d2d7a1a36645ba
“kVadm” <1408vk@gmail.com>
2017-01-21 21:55:31 +0200

    first commit

---
+++ RobotBobot/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace RobotBobot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_RUN = new System.Windows.Forms.Button();
+            this.button_STOP = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_RUN
+            // 
+            this.button_RUN.Location = new System.Drawing.Point(41, 30);
+            this.button_RUN.Name = "button_RUN";
+            this.button_RUN.Size = new System.Drawing.Size(92, 23);
+            this.button_RUN.TabIndex = 0;
+            this.button_RUN.Text = "RUN";
+            this.button_RUN.UseVisualStyleBackColor = true;
+            this.button_RUN.Click += new System.EventHandler(this.button_RUN_Click);
+            // 
+            // button_STOP
+            // 
+            this.button_STOP.Location = new System.Drawing.Point(153, 30);
+            this.button_STOP.Name = "button_STOP";
+            this.button_STOP.Size = new System.Drawing.Size(89, 23);
+            this.button_STOP.TabIndex = 0;
+            this.button_STOP.Text = "STOP";
+            this.button_STOP.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(260, 200);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 391);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_STOP);
+            this.Controls.Add(this.button_RUN);
+            this.Name = "Form1";
+            this.Text = "RB";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_RUN;
+        private System.Windows.Forms.Button button_STOP;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ RobotBobot/RestClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotBobot
+{
+    public static class RestClient
+    {
+        public static void GetData()
+        {
+            for (int i =0; i<20; i++)
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://data.btcchina.com/data/orderbook?limit=10");
+                request.Method = "GET";
+                request.Accept = "application/json";
+                request.KeepAlive = false;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                StringBuilder output = new StringBuilder();
+                output.Append(reader.ReadToEnd());
+
+                string data = output.ToString();
+                Console.WriteLine(data);
+
+                RootObject rootObject = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(data);
+                double asks = rootObject.asks.Average(innerList => innerList[0]);
+                Console.WriteLine(asks);
+                double bids = rootObject.bids.Average(innerList => innerList[0]);
+                Console.WriteLine(bids);
+            }         
+        }
+
+        public class RootObject
+        {
+            public List<List<double>> asks { get; set; }
+            public List<List<double>> bids { get; set; }
+            public int date { get; set; }
+        }
+
+    }
+}
