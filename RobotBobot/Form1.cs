a404738bb3829c90a5e97154f07c0c12ab480075
“kVadm” <1408vk@gmail.com>
2017-02-01 00:18:16 +0200

    first table

--- RobotBobot/Form1.cs
+++ RobotBobot/Form1.cs
@@ -5,8 +5,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -31,6 +33,9 @@ namespace RobotBobot
         {
             button_STOP.Enabled = true;
             button_RUN.Enabled = false;
+            //GUI.InitializeWebDriver();
+            //GUI.Login();
+            //Thread.Sleep(5000);
             if (backgroundWorker1.IsBusy != true)
             {
                 backgroundWorker1.RunWorkerAsync();
@@ -41,6 +46,7 @@ namespace RobotBobot
         {
             button_RUN.Enabled = true;
             button_STOP.Enabled = false;
+
             if (backgroundWorker1.WorkerSupportsCancellation == true)
             {
                 backgroundWorker1.CancelAsync();
@@ -51,7 +57,7 @@ namespace RobotBobot
 
         private void backgroundWorker1_DoWork_1(object sender, DoWorkEventArgs e)
         {
-            GUI.GetDataGUI();
+            
             BackgroundWorker worker = sender as BackgroundWorker;
 
             while (true)
@@ -63,16 +69,16 @@ namespace RobotBobot
                 }
                 else
                 {
-                   // RestClient.GetData();
-                    
-                    Thread.Sleep(300);
-                    //  worker.ReportProgress(10);
+                   RestClient.GetData();
+                   //  GUI.GetOrderPrice();
+                   // Thread.Sleep(100);
                 }
             }
         }
 
         private void backgroundWorker1_RunWorkerCompleted_1(object sender, RunWorkerCompletedEventArgs e)
         {
+            //GUI.CloseApp();
             if (e.Cancelled == true)
             {
                 //resultLabel.Text = "Canceled!";
@@ -96,21 +102,76 @@ namespace RobotBobot
     }
 
     public static class GUI {
+
         public static IWebDriver driver;
         public static IJavaScriptExecutor js;
-     public static void GetDataGUI()
-        {
 
+        #region read elements from page ---
+
+        static public IWebElement emeil { get { return driver.FindElement(By.Name("email")); } }
+        static public IWebElement password { get { return driver.FindElement(By.Name("password")); } } 
+        static public IWebElement trade { get { return driver.FindElement(By.CssSelector("div>a[href='https://www.btcchina.com/exc/trade/cnybtc']")); } }
+
+        static public IWebElement Asks_BTCChina { get { return driver.FindElement(By.CssSelector(".asks")); } }
+        static public IWebElement Bids_BTCChina { get { return driver.FindElement(By.CssSelector(".bids")); } }
+
+        #endregion
+
+        public static void InitializeWebDriver()
+        {
 
             var chromeDriverService = ChromeDriverService.CreateDefaultService();
             chromeDriverService.HideCommandPromptWindow = true;
             driver = new ChromeDriver(chromeDriverService, new ChromeOptions());
-            driver.Navigate().GoToUrl("https://bitcoinwisdom.com/");
+            driver.Navigate().GoToUrl("https://www.btcchina.com");
             driver.Manage().Window.Maximize();
 
             js = (IJavaScriptExecutor)driver;
 
-
         }
+
+         public static void CloseApp()
+         {
+             driver.Quit();
+         }
+
+         public static void Login()
+         {
+
+             emeil.SendKeys("BTC960882@gmail.com");
+             password.SendKeys("AB288069");
+             password.SendKeys(OpenQA.Selenium.Keys.Enter);
+             Thread.Sleep(8000);
+             trade.Click();
+
+         }
+         public static void GetOrderPrice()
+         {
+             string currentAsks = Asks_BTCChina.Text;
+             string currentBids = Bids_BTCChina.Text;
+
+             Logger.Log.Info(String.Format("{0} : {1}", AvarageValues(currentAsks), AvarageValues(currentBids)));
+         }
+
+         public static string AvarageValues(string row)
+         {
+             string[] values = Regex.Split(row, "\r\n");
+             string[] a = new string[] { values[0].Substring(0, 5), 
+                                         values[3].Substring(0, 5),
+                                         values[6].Substring(0, 5), 
+                                         values[9].Substring(0, 5), 
+                                         values[12].Substring(0, 5) 
+                                       };
+             string[] b = new string[] { values[1], values[4], values[7], values[10], values[13] };
+             string[] c = new string[] { values[2], values[5], values[8], values[11], values[14] };
+             double[] d = Array.ConvertAll(a, s => double.Parse(s));
+             double[] e = Array.ConvertAll(b, s => double.Parse(s, CultureInfo.InvariantCulture));
+             double[] h = Array.ConvertAll(c, s => double.Parse(s, CultureInfo.InvariantCulture));
+             double average1 = d.Average();
+             double average2 = e.Average();
+             double average3 = h.Average();
+
+             return String.Format("{0} : {1} : {2}", average1, average2, average3);
+         }
     }
 }
--- RobotBobot/RestClient.cs
+++ RobotBobot/RestClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -7,37 +8,67 @@ using System.Text;
 namespace RobotBobot
 {
     public static class RestClient
-    {
+    {      
         public static void GetData()
         {
-            HttpWebRequest request;
-            HttpWebResponse response;
-            StreamReader reader;
-            StringBuilder output;
-
-            for (int i = 0; i < 10; i++)
-            {
-                request = (HttpWebRequest)WebRequest.Create("https://data.btcchina.com/data/orderbook?limit=10");
-                request.Method = "GET";
-                request.Accept = "application/json";
-                request.KeepAlive = false;
-                response = (HttpWebResponse)request.GetResponse();
-
-                reader = new StreamReader(response.GetResponseStream());
-                output = new StringBuilder();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://data.btcchina.com/data/orderbook?limit=20");
+            request.Method = "GET";
+            request.Accept = "application/json";
+            request.KeepAlive = false;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            StringBuilder output = new StringBuilder();
                 output.Append(reader.ReadToEnd());
 
                 string data = output.ToString();
                 Logger.Log.Info(data);
 
-                RootObject rootObject = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(data);
-                double asks = rootObject.asks.Average(innerList => innerList[0]);
-                double bids = rootObject.bids.Average(innerList => innerList[0]);
-                Logger.Log.Info("Avarage asks- " + asks);
-                Logger.Log.Info("Avarage bids- " + bids);
+             RootObject rootObject = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(data);
+               // double asks = rootObject.asks.Average(innerList => innerList[0]);
+               // double bids = rootObject.bids.Average(innerList => innerList[0]);
 
-            }
+                double bids1 = rootObject.bids[0][0];
+                double bids_BTC_1 = Math.Round(GetBTC(1, rootObject.bids), 2);
+                double bids_BTC_5 = Math.Round(GetBTC(5, rootObject.bids), 2);
+                double bids_BTC10 = Math.Round(GetBTC(10, rootObject.bids), 2);
+                double bids_BTC20 = Math.Round(GetBTC(20, rootObject.bids), 2);
+
+                rootObject.asks.Reverse();
+                
+                double asks1 = rootObject.asks[0][0];
+                double asks_BTC_1 = Math.Round(GetBTC(1, rootObject.asks), 2);
+                double asks_BTC_5 = Math.Round(GetBTC(5, rootObject.asks), 2);
+                double asks_BTC10 = Math.Round(GetBTC(10, rootObject.asks), 2);
+                double asks_BTC20 = Math.Round(GetBTC(20, rootObject.asks), 2);
+
+                Logger.Log.Info("");
+                Logger.Log.Info("______BIDS: " + bids1 + " " + bids_BTC_1 + " " + bids_BTC_5 + " " + bids_BTC10 + " " + bids_BTC20 + " ______ASKS: " + asks1 + " " + asks_BTC_1 + " " + asks_BTC_5 + " " + asks_BTC10 + " " + asks_BTC20);               
         }
+        public static double GetBTC(int depth, List<List<double>> spot)
+        {
+            double diff = depth;
+            int nubmer = depth;
+            double BTC = 0;
+            int i = 0;
+
+            while (diff >= 0)
+            {
+                diff = diff - spot[i][1];
+                i++;
+            }
+            for (int x = 0; x < i - 1; x++)
+            {
+                BTC = BTC + spot[x][0] * spot[x][1];
+            }
+
+            double left = (spot[i - 1][1] + diff);
+            BTC = BTC + spot[i - 1][0] * left;
+            BTC = BTC / nubmer;
+
+            return BTC;
+        }
+
+
 
         public class RootObject
         {
